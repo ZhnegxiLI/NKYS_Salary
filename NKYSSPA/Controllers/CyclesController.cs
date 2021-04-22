77123b605f93661d96543f84391278a1626db243
martin <zhengxi.li.19@neoma-bs.com>
2021-04-22 23:45:58 +0200

    Dev: add create Cycle

--- NKYSSPA/Controllers/CyclesController.cs
+++ NKYSSPA/Controllers/CyclesController.cs
@@ -27,5 +27,24 @@ namespace NKYSSPA.Controllers
             return await _context.Cycle.ToListAsync();
         }
 
+        [HttpPost]
+        public async Task<long> CreateOrEdit(Cycle cycle)
+        {
+            cycle.Year = cycle.FromDate.Year;// place into sql trigger 
+            cycle.Month = cycle.FromDate.Month; // place into sql trigger
+            if (cycle.Id>0)
+            {
+                // Update
+                _context.Cycle.Update(cycle);
+            }
+            else
+            {
+                cycle.CreatedOn = DateTime.Now;
+                await _context.Cycle.AddAsync(cycle);
+                // Add
+            }
+            await _context.SaveChangesAsync();
+            return cycle.Id;
+        }
     }
 }
